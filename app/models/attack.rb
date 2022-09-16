2ebeac43b95304f385bf52aafcfaf7c856e3f864
tincho-info <tincho.info@gmail.com>
2022-09-16 12:40:06 +0000

    cambios en los modelos

--- app/models/attack.rb
+++ app/models/attack.rb
@@ -1,2 +1,7 @@
 class Attack < ApplicationRecord
+    validate restriccion
+    def restriccion 
+        if Monster.find_by(id: )
+            monster_
+    end
 end
--- app/models/monster.rb
+++ app/models/monster.rb
@@ -1,3 +1,3 @@
 class Monster < ApplicationRecord
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true, uniqueness: true        
 end
